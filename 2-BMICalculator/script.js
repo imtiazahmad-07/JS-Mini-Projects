c6fac3254d725dd614ffb01fce87a4165b480cc9
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-11-01 11:23:32 -0700

    udpated main html file

---
+++ 2-BMICalculator/script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("form");
+form.addEventListener("submit",()=>{
+    let height = parseInt(document.querySelector("#height"));
+    let weight = parseInt(document.querySelector("#weight").value);
+
+})
